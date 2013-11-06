df75c92115a7c88a9953b02c7cf262e5330da11f
mikach <mikach27@gmail.com>
2013-11-06 14:35:22 +0200

    Add tests

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ describe('detect promises and generators', function() {
         expect( Y.isGenerator(gen) ).to.be.false;
         expect( Y.isGenerator(gen()) ).to.be.true;
     });
-    
+
     it('isGeneratorFn', function() {
         expect( Y.isGeneratorFn(gen) ).to.be.true;
         expect( Y.isGeneratorFn(readFile) ).to.be.false;
@@ -34,9 +34,42 @@ describe('detect promises and generators', function() {
 });
 
 describe('Y function', function() {
+    var gen = function* () {
+        for (var i = 0; ++i < 10;)
+            yield i;
+        return i;
+    };
 
+    it('should return function', function() {
+        var b = Y(gen);
+        expect(b).to.be.a('function');
+    });
+
+    it('should return self if function is not a generator', function() {
+        var emptyFn = function() {};
+        expect( Y(emptyFn) ).to.be.equal( emptyFn );
+    });
+
+    it('should return value', function() {
+        expect( Y(gen)() ).to.be.equal( 10 );
+    });
+
+    it('once()', function() {
+
+    });
 });
 
 describe('nodejs functions wrappers', function() {
-
+    describe('nwrap()', function() {
+        var read = Y.nwrap(fs.readFile);
+        it('return promise', function() {
+            expect( Y.isPromise(read()) ).to.be.true;
+        });
+        it('read a file', function(done) {
+            read('test/example.txt', 'utf-8').then(function(content) {
+                expect('content').to.be.a('string');
+                done();
+            });
+        });
+    });
 });
