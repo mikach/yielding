61b4544ce0d90bf9f8130db5237c955369b54ce1
mikach <mikach27@gmail.com>
2013-11-18 11:50:20 +0200

    Add examples/throw.js

---
+++ examples/throw.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var Y = require('..');
+var Q = require('q');
+
+
+Y(function* () {
+    try { 
+      var content = yield Y.ncall(fs.readFile, 'non_exists_file', 'utf-8');
+    } catch(e) {
+      console.log('ERROR: ' + e.message);
+    }
+    console.log('done');
+})();
