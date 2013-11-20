126338d742af368d31a12fc7f38f37e157fca0ee
mikach <mikach27@gmail.com>
2013-11-20 10:59:31 +0200

    Add examples/parallel.js

---
+++ examples/parallel.js
@@ -0,0 +1,10 @@
+var Y = require('..');
+var get = Y.nwrap( require('request').get );
+
+Y(function* () {
+    var pages = ['http://google.com', 'http://yahoo.com', 'http://bind.com'];
+    var content = yield pages.map(function(url) {
+        return get(url);
+    });
+    console.log(content.map(function(c) { return c.body.length; }));
+})();
